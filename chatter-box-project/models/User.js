bd5fa433e7462c64ab63b8792e2a7bb29bc442c1
Tyler <35749981+xlinked@users.noreply.github.com>
2021-10-25 11:26:15 -0700

    Add files via upload

---
+++ chatter-box-project/models/User.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
+const Schema = mongoose.Schema;
+
+const userSchema = new mongoose.Schema ({
+  name: [{
+    type: String,
+    required: [true, 'Please enter a name']
+  }],
+  phone: {
+    type: String,
+    required: [true, 'Please enter a Phone Number']
+  },
+  messages: [{
+    type: String
+  }],
+  date: { 
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  img: {
+    data: Buffer,
+    contentType: String
+  },
+  email: {
+    type: String,
+    required: [true, 'Please enter an email'],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, 'Please enter a valid email']
+  },
+  password: {
+    type: String,
+    required: [true, 'Please enter a password'],
+    minlength: [8, 'Minimum password length is 8 characters']
+  }
+});
+
+
+// fire a function before doc saved to db
+userSchema.pre('save', async function(next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// static method to login user
+userSchema.statics.login = async function(email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
+    }
+    throw Error('incorrect password');
+  }
+  throw Error('incorrect email');
+};
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
